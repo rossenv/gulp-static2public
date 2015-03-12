d736d4239b773a6b2ef1dc1ca52ad7ed51d00ae3
Rossen Vassilev <rvasilev@despark.com>
2015-03-12 12:16:45 +0200

    Gulp updated
    Bourbon & Neat added

--- gulp/build.js
+++ gulp/build.js
@@ -7,74 +7,87 @@ var inject = require('gulp-inject'),
 		data = require('gulp-data'),
 		debug = require('gulp-debug');
 
+var neat = require('node-neat').includePaths;
+var bourbon = require('node-bourbon').includePaths
+
+
 var $ = require('gulp-load-plugins')({
-	pattern: ['gulp-*', 'main-bower-files', 'uglify-save-license', 'del']
+    pattern: ['gulp-*', 'main-bower-files', 'uglify-save-license', 'del']
 });
 
-function handleError(err) {
-	notify().write('\nERROR IN SASS ---------------\n'+err.message+'\n /ERROR ---------------');
-	this.emit('end');
+function handleCSSError(err) {
+    notify().write('\nERROR IN SASS ---------------\n'+err.message+'\n /ERROR ---------------');
+    this.emit('end');
 }
 
 gulp.task('styles', ['wiredep'],  function () {
-	return gulp.src('static_html/scss/**/*.scss')
-		.pipe(sourcemaps.init())
-		.pipe($.sass({style: 'expanded'}))
-		.on('error', handleError)
-		.pipe($.autoprefixer('last 1 version'))
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('public/css'))
-		.pipe($.size());
+    return gulp.src('static_html/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe($.sass({
+            outputStyle: 'compressed',
+            // includePaths: require('node-bourbon').includePaths
+            includePaths: bourbon.concat(neat)
+        }))
+        .on('error', handleCSSError)
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('public/css'))
+        .pipe($.size());
 });
 
 gulp.task('js', function () {
-	return gulp.src('static_html/js/**/*.js')
-		.pipe($.jshint())
-		.pipe($.jshint.reporter('jshint-stylish'))
-		// .pipe(gulp.dest('public/js'))
-		.pipe($.size());
+    return gulp.src('static_html/js/**/*.js')
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish'))
+        .pipe(gulp.dest('public/js'))
+        .pipe($.size());
 });
 
+gulp.task('partials', function () {
+  return gulp.src('static_html/partials/**/*.html')
+    .pipe(gulp.dest('public/partials'))
+    .pipe($.size());
+});
 
 gulp.task('html', function () {
-	var assets;
-	return gulp.src(['static_html/*.html', 'static_html/partials/**/*.html'])
-		.pipe(assets = $.useref.assets())
-		.pipe(assets.restore())
-		.pipe($.useref())
-		.pipe(gulp.dest('public'))
-		.pipe($.size());
+    var assets;
+    return gulp.src('static_html/*.html')
+        .pipe(assets = $.useref.assets())
+        .pipe(assets.restore())
+        .pipe($.useref())
+        .pipe(gulp.dest('public'))
+        .pipe($.size());
 });
 
 gulp.task('clear-cache', function (done) {
-		return $.cache.clearAll(done);
+        return $.cache.clearAll(done);
 });
 
 gulp.task('images', function () {
-	return gulp.src('static_html/img/**/*')
-		.pipe($.cache($.imagemin({
-			optimizationLevel: 3,
-			progressive: true,
-			interlaced: true
-		})))
-		.pipe(gulp.dest('public/img'))
-		.pipe($.size());
+    return gulp.src('static_html/img/**/*')
+        .pipe($.cache($.imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('public/img'))
+        .pipe($.size());
 });
 
 gulp.task('fonts', function () {
-	return gulp.src('static_html/fonts/*.{eot,svg,ttf,woff}')
-		.pipe(gulp.dest('public/css/fonts'))
-		.pipe($.size());
+    return gulp.src('static_html/fonts/*.{eot,svg,ttf,woff}')
+        .pipe(gulp.dest('public/css/fonts'))
+        .pipe($.size());
 });
 
 gulp.task('misc', function () {
-	return gulp.src('static_html/*.{ico,json}')
-		.pipe(gulp.dest('public'))
-		.pipe($.size());
+    return gulp.src(['static_html/**/*.{ico,json}'])
+        .pipe(gulp.dest('public'))
+        .pipe($.size());
 });
 
+
 gulp.task('clean', function (done) {
-	$.del('public', done);
+    $.del('public', done);
 });
 
 gulp.task('update-readme', function () {
@@ -90,13 +103,13 @@ gulp.task('update-readme', function () {
 
 				if (undefined != dep) {
 					for (var prop in dep) {
-						strDep += '<p>' + prop + ': ' + dep[prop] + '</p>';
+						strDep += prop + ': ' + dep[prop] + '\n';
 					}
 				}
 
 				if (undefined != devDep) {
 					for (var prop in devDep) {
-						strDep += '<p>' + prop + ': ' + devDep[prop] + '</p>';
+						strDep += prop + ': ' + devDep[prop] + '\n';
 					}
 				}
 
@@ -108,4 +121,4 @@ gulp.task('update-readme', function () {
 	.pipe($.size());
 });
 
-gulp.task('build', ['styles', 'js', 'html', 'images', 'fonts', 'misc', 'update-readme']);
+gulp.task('build', ['styles', 'js', 'html', 'partials', 'images', 'fonts', 'misc', 'update-readme']);
